26bfc1c9ee97a72b17621fb45d1ae270eebe9830
grojas <grojas@dwsoftware.mx>
2016-10-17 09:02:59 -0500

    4.- Router Links: Se modificaron los links en app.component para redireccionar correctamente a los componentes correctos y se movio html a archivo separado

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,38 +5,7 @@ import { ContactComponent }  from "./contact/contact.component";
 
 @Component({
     selector: "my-app",
-    styles: [`
-
-  `],
-    template: `
-    <header>
-        <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a href="#" class="navbar-brand">My Routing App</a>
-                </div>
-
-                <ul class="nav navbar-nav">
-                    <li><a href="#">Home1</a></li>
-                    <li><a href="#">Home2</a></li>
-                    <li><a href="#">Home3</a></li>
-                </ul>
-            </div>
-        </nav>
-    </header>
-
-    <main>
-      <router-outlet></router-outlet>
-    </main>
-    <!--  <div class="jumbotron text-center">
-        <h1>The App Lives!</h1>
-        <p>{{ message }}</p>
-      </div> -->
-
-<!--      <home-page></home-page>
-      <about-page></about-page>
-      <contact-page></contact-page> -->
-  `
+    templateUrl: "./app/app.component.html"
 })
 export class AppComponent {
     message = "This is the sample message.";
