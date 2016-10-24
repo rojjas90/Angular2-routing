f1d873851a1b3bee7b721b8b769d423541d608e6
grojas <grojas@dwsoftware.mx>
2016-10-24 12:32:29 -0500

    13.- About Module: Creacion de modulo para seccion About con NgModule, uso de CommonModule para usar directivas ng , en componente hijo

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,20 +2,16 @@ import { ModuleWithProviders }  from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent }  from "./home/home.component";
 import { ContactComponent }  from "./contact/contact.component";
-import { AboutComponent } from "./about/about.component";
-import { AboutUserComponent }  from "./about/about-user.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 
 const appRoutes: Routes = [
-    // { path: "", component: HomeComponent },
-    {
-        path: "",
-        redirectTo: "/about",
-        pathMatch: "full"
-    },
-    { path: "about", component: AboutComponent },
+    { path: "", component: HomeComponent },
+    // {
+    //     path: "",
+    //     redirectTo: "/about",
+    //     pathMatch: "full"
+    // },
     { path: "contact", component: ContactComponent },
-    { path: "about/:username", component: AboutUserComponent },
     { path: "**", component: NotFoundComponent }
 ];
 
