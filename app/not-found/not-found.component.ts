b7dcf093b8ed8a938d6739eda76b5ae377cf1116
grojas <grojas@dwsoftware.mx>
2017-02-14 17:39:05 -0600

    13.mine Ignore files
    We've added new lines in .gitignore file to ignore directory and some kind of files such as:
    dist
    *.js.map

--- app/not-found/not-found.component.ts
+++ app/not-found/not-found.component.ts
@@ -3,7 +3,7 @@ import { Component }  from "@angular/core";
 @Component({
   template: `
   <div class="jumbotron text-center">
-    <h2>404 Not found</h2>
+    <h1>404 Not found</h1>
     <p>You be lost. Follow the breadcrumbs back <a routerLink="/">Home</a>.</p>
   </div>
   `
