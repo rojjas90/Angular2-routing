0596ba04f8e619b01ad26413673d975d520ff905
grojas <grojas@dwsoftware.mx>
2017-02-15 09:59:25 -0600

    14.- Child Routes
    We've created about-section and called it in about.routing and about.module, we re-defined child routes in about.routing

---
+++ app/about/about-section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    template: `
+    <h2 class="text-center">About Section</h2>
+  <router-outlet><router-outlet>
+  `
+})
+export class AboutSectionComponent { }
--- app/about/about.routing.ts
+++ app/about/about.routing.ts
@@ -2,10 +2,25 @@ import { ModuleWithProviders }  from "@angular/core";
 import { Routes, RouterModule  }  from "@angular/router";
 import { AboutComponent } from "./about.component";
 import { AboutUserComponent }  from "./about-user.component";
+import { AboutSectionComponent } from "./about-section.component";
 
 const aboutRoutes: Routes = [
-    { path: "about", component: AboutComponent },
-    { path: "about/:username", component: AboutUserComponent }
+    {
+        path: "about",
+        component: AboutSectionComponent,
+        children: [
+            { // whenever we declare children, we always we have to have a path has a blank in it: path: "" ...
+                path: "",
+                component: AboutComponent
+            },
+            {
+                path: ":username",
+                component: AboutUserComponent
+            }
+        ]
+    }
+    // { path: "about", component: AboutComponent },
+    // { path: "about/:username", component: AboutUserComponent }
 ];
 
 export const aboutRouting: ModuleWithProviders = RouterModule.forChild(aboutRoutes);
